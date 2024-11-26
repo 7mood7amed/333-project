54c552fc8d3808c945a1a4d80c0f677b4414551a
7mood7amed <mm7526347@gmail.com>
2024-11-26 22:49:45 +0300

    modifications

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php
+
+// checking the request method
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    // getting inputs submitted from the form
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
+
+    // validating UOB email
+    $pattern = "/@uob\.edu\.bh/";
+    if(!preg_match($pattern ,$email)){
+        die("Invalid email address. Only UOB email (@uob.edu.bh) are allowed");
+    }
+
+    // hashing the password
+    $hashed_password = password_hash($password,PASSWORD_DEFAULT);
+
+    // message after successful registration
+    echo "Registereation successful for $name! head to login page ";
+
+    // redirect to login page after successful registration
+    header("Location: login.html");
+    exit();
+}
+
+?>
